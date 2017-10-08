24959d2c51f7a53f955eeefd65d04e086aaeafc0
Naif6003 <e7sas_nano@hotmail.com>
2017-10-08 16:13:12 -0700

    starting the project

---
+++ src/SDES/SDESimplementation.java
@@ -0,0 +1,32 @@
+package SDES;
+
+public class SDESimplementation {
+
+	// list of the keys provided in the question
+	
+	private final static byte[] KEYS =
+		{
+	        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	        1, 1, 1, 0, 0, 0, 1, 1, 1, 0,
+	        1, 1, 1, 0, 0, 0, 1, 1, 1, 0,
+	        1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+	    };
+	
+//	public static byte[] Encrypt(byte[] rawkey, byte[] plaintext){
+//		
+//		
+//		
+//		
+//		
+//		return;
+//	}
+//	
+//	public static byte[] Decrypt(byte[] rawkey, byte[] ciphertext){
+//		
+//		
+//		
+//		
+//		
+//		return;
+//	}
+}
